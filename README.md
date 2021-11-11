f7165419e3e6133e33e903adcf435692e30880cc
Nicolas Marien <nmarien29@gmail.com>
2021-11-11 18:55:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+The Citadel Colors app is great, but it lacks the ability to sync the inventory and wishlist between devices.
+
+This script will allow you to load a Notion database with the existing paints, so that you can use notion relations to create a synced inventory and wishlist.
+
+You'll need to create a Notion Integration and a database dedicated to this procedure.
+See [the Notion API documentation](https://developers.notion.com/reference/intro) to get started.
+
+The JSON data comes from the official [Citadel Colors website](https://www.games-workshop.com/en-GB/detail?N=3815391097+2401632303&searchTerm=paint).
+It has been extracted by running the following Javascript snippet `var paints = [...$$('.record-spotlight__item-image')].reduce((map, paint) => { map[paint.alt] = paint.src; return map; }, {})` on each paint type tab. 
+
+To run the script, please paste the following command into your terminal (set the proper values for your workspace): `PAINT_DATABASE_API=CHANGE_ME NOTION_BEARER=CHANGE_ME node index.js`.
---
+++ index.js
@@ -0,0 +1,51 @@
+const { Client } = require('@notionhq/client')
+const fs = require('fs')
+const notion = new Client({ auth: process.env.NOTION_BEARER });
+
+function toTitleCase (string) {
+  const parts = string.split('');
+  parts[0] = parts[0].toUpperCase();
+  return parts.join('');
+}
+(async () => {
+  const databaseID = process.env.PAINT_DATABASE_API;
+
+  const files = fs.readdirSync(__dirname + "/paints");
+
+  for (const json of files) {
+    const paints = require(__dirname + '/paints/' + "shade.json");
+    const tag = json.split('.')[0];
+    for (const [name, url] of Object.entries(paints)) {
+
+      const page = await notion.pages.create({
+        parent: {
+          database_id: databaseID
+        },
+        icon: {
+          type: "external",
+          external: {
+            url
+          }
+        },
+        properties: {
+          "Name": {
+            title: [
+              {
+                text: {
+                  content: `${toTitleCase(tag)}: ${name}`
+                }
+              }
+            ]
+          },
+          Type: {
+            select: {
+              name: tag
+            }
+          }
+
+        }
+      });
+      console.info(`created page ${page.id}`);
+    }
+  } 
+})();
